4c172cc145c41a50324a0e07f34ff6a70f7a562a
EC2 Default User <ec2-user@ip-172-31-13-251.ap-northeast-1.compute.internal>
2023-07-19 13:02:57 +0000

    Initial Commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,10 +6,11 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    if book.save
-    　flash[:notice] = "Book was successfully created."
-    　redirect_to book_path(book.id)
+    @books = Book.all
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
     else
       render :index
     end
@@ -24,14 +25,20 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+   if @book.save
+    flash[:notice] = "Book was successfully created."
+    redirect_to book_path(@book.id)
+   else
+      render :edit
+   end
   end
   
   def destroy
     book = Book.find(params[:id])
     book.destroy
+    flash[:notice] = "Book was successfully created."
     redirect_to '/books'
   end
     
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
-  get 'top' => 'homes#top'
   get 'books/:id' => 'books#show', as: 'book'
   patch 'books/:id' => 'books#update', as: 'update_book'
   resources :books
